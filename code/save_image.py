0254ae76723144a1bc73e662d54b995de0d0801e
Varun0157 <varun.edachali@gmail.com>
2024-04-07 15:48:50 +0530

    reformat: improved quality, decreased length

--- code/save_image.py
+++ code/save_image.py
@@ -1,8 +1,10 @@
 import pybullet as p
 from PIL import Image
 
+RES = (2048, 2048)
+
 def save_image(pb: p, index: int) -> None:
-    img = pb.getCameraImage(1024, 1024, renderer=pb.ER_BULLET_HARDWARE_OPENGL)
+    img = pb.getCameraImage(*RES, renderer=pb.ER_BULLET_HARDWARE_OPENGL)
     rgbBuffer = img[2]
     rgbim = Image.fromarray(rgbBuffer)
     rgbim.save(f"./img/rgbimage_{index}.png")
