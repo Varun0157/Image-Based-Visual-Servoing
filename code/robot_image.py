b807cb1047b6dd9d7e11a33fb590cd31ebcbadb7
Varun0157 <varun.edachali@gmail.com>
2024-04-28 04:12:37 +0530

    bug: removed unnecessary import

--- code/robot_image.py
+++ code/robot_image.py
@@ -1,5 +1,4 @@
 from PIL import Image, ImageDraw
-from matplotlib.pylab import f
 import numpy as np
 from typing import List, Tuple
 
@@ -32,8 +31,6 @@ def save_rgb_image(img_arr: np.ndarray, img_path: str) -> None:
     img.save(img_path)
 
 
-def save_with_error(
-    img_path: str, img_arr: np.ndarray, index: int, error: float
-) -> None:
+def save_with_error(img_path: str, img_arr: np.ndarray, error: float) -> None:
     save_rgb_image(img_arr, img_path)
     write_text_to_image(img_path, f"error: {error}", (10, 10), "red")
